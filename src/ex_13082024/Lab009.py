b5c91de514c4c48ecd88ffede06ab0fe2b4686b7
Ravi <mravi7133@gmail.com>
2024-08-15 12:15:38 +0530

    Lab 09 aug 2024 added

--- src/ex_13082024/Lab009.py
+++ src/ex_13082024/Lab009.py
@@ -10,6 +10,9 @@ print(type(a))
 print(type(age))
 how_many_plain_i_have = None
 print(type(how_many_plain_i_have))
-#ID FUNCTION
+#ID FUNCTIONS
 age = 10
-print(id(age)) # id place in memory
+age2 = 111
+
+print(id(age)) # id place in memory 140725190134488
+print(id(age2)) # id place in memory 140725190134488
